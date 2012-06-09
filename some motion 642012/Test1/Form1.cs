f9b26e3a0ac819356db332d9711d5d79930d1b7a
John Loftus <jloftus@luzerne.edu>
2012-06-09 17:34:33 -0400

    First commit of Some motion 642012

---
+++ some motion 642012/Test1/Form1.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+// 
+// J. Loftus June 2012, for Luzerne County Community College.
+// Demonstration code created as part of a weekly workshop offered at LCCC during the Summer
+// of 2012 by J. Loftus, entitled "Applied Data Structures and Algorithms for Interactive Simulation Systems"
+// Code demonstrates how to randomly set direction in response to a System.timer event, and how to redraw
+// the walls of the maze as the game element moves along.  (This is done in yellow so one can see what's happening.)
+// 
+namespace Test1
+{
+    public partial class Form1 : Form
+    {
+        Graphics paper;
+        int rw, cl, x, y;
+        Grid grid;
+        public Random ran;
+        public static bool toggle;
+        public static int times;
+        public static int DY;
+        public static int DX;
+        public Rectangle cr, oldrect, newrect;
+        bool stop, resetflag;
+        Pen blackPen; Pen yellowPen;
+        public int direction;
+        public Form1()
+        {
+            InitializeComponent();
+            toggle = true; times = 0;
+            DY = 20; DX = 20; stop = false;
+            ran = new Random();
+            rw = 0; cl = 0; x = 0; y = 0; direction = 1;
+            resetflag = true;
+        }
+
+        // When the panel in which the Grid of squares is being drawn is hit with a Paint event,
+        // then we call drawGrid to redraw the maze. 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            paper = e.Graphics;
+            SolidBrush bluebrush = new SolidBrush(Color.Blue);
+            SolidBrush yellowbrush = new SolidBrush(Color.Yellow);
+            SolidBrush greyBrush = new SolidBrush(Color.DimGray);
+            blackPen = new Pen(Color.Black); yellowPen = new Pen(Color.Yellow);
+            if (resetflag == true)
+            {
+               
+                grid = new Grid();
+                grid.drawGrid(paper);
+                                       
+            }
+            if (resetflag == false)
+            {
+                switch (direction)
+                {
+                    case 0:  // up
+                            oldrect = new Rectangle(x+3,y+3, 14, 14);
+                            newrect = new Rectangle(x+3, y-17, 14, 14);
+                            paper.FillEllipse(greyBrush, oldrect);
+                            paper.FillEllipse(bluebrush, newrect);
+                            drawWall(x, y-20, yellowPen); drawWall(x, y, yellowPen);
+                            rw--; y = y - 20;
+                            break;
+                    case 1:  // down
+                            oldrect = new Rectangle(x+3, y+3, 14, 14);
+                            newrect = new Rectangle(x+3, y + 23, 14, 14);
+                            paper.FillEllipse(greyBrush, oldrect);
+                            paper.FillEllipse(bluebrush, newrect);
+                            drawWall(x, y + 20, yellowPen); drawWall(x, y, yellowPen);
+                            rw++; y = y + 20;
+                            break;
+                    case 2:  // left
+                            oldrect = new Rectangle(x+3, y+3, 14, 14);
+                            newrect = new Rectangle(x-17, y+3, 14, 14);
+                            paper.FillEllipse(greyBrush, oldrect);
+                            paper.FillEllipse(bluebrush, newrect);
+                            drawWall(x-20, y, yellowPen); drawWall(x, y, yellowPen);
+                            cl--; x = x - 20;
+                            break;
+                    case 3:  // right
+                            oldrect = new Rectangle(x+3, y+3, 14, 14);
+                            newrect = new Rectangle(x+23, y+3, 14, 14);
+                            paper.FillEllipse(greyBrush, oldrect);
+                            paper.FillEllipse(bluebrush, newrect);
+                            drawWall(x+20, y, yellowPen); drawWall(x, y, yellowPen);
+                            cl++; x = x + 20;
+                            break;
+                }  // end of switch
+                    
+                                                
+              
+            }  // if resetflag == false
+
+            resetflag = false;
+        }  // end of paint
+
+        private void drawWall(int X, int Y, Pen p)
+        {
+            int i = Y/20; int j = X/20;
+            // up
+            if (  (Y > 0) & (!grid.gridRec[i,j].getUp())  )
+            {
+                Point pt1 = new Point(X, Y);
+                Point pt2 = new Point(X+20, Y);
+                paper.DrawLine(p, pt1, pt2);
+            }
+            // down
+            if (  (Y < 480) & (!grid.gridRec[i,j].getDown())  )
+            {
+                Point pt1 = new Point(X, Y+20);
+                Point pt2 = new Point(X + 20, Y+20);
+                paper.DrawLine(p, pt1, pt2);
+            }
+            // left
+            if (  (X > 0) & (!grid.gridRec[i,j].getLeft())   ) 
+            {
+                Point pt1 = new Point(X, Y);
+                Point pt2 = new Point(X, Y+20);
+                paper.DrawLine(p, pt1, pt2);
+            }
+            // right
+            if (  (X < 480) & (!grid.gridRec[i,j].getRight())  ) 
+            {
+                Point pt1 = new Point(X+20, Y);
+                Point pt2 = new Point(X+20, Y+20);
+                paper.DrawLine(p, pt1, pt2);
+            }
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            ran = new Random();
+            rw = 0; cl = 0; x = 0; y = 0; direction = 1;
+            resetflag = true;
+            panel1.Refresh();
+        }
+        
+        private void label2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Goodbye!");
+            Close();
+        }
+       
+         
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+            bool goflag = false; resetflag = false;
+            if (ran.Next(3) == 1) direction = ran.Next(4);
+            do
+            {
+                
+                switch (direction)
+                {
+                    case 0:  // up
+                        if ((y > 0) & (grid.gridRec[rw, cl].getUp()) )
+                        {
+                            cr = new Rectangle(x, y - 20, 20, 40);                   
+                            goflag = true;
+                        }
+                        break;
+                    case 1:  // down
+                        if ((y < 480) & (grid.gridRec[rw, cl].getDown()))
+                        {
+                            cr = new Rectangle(x, y, 20, 40);                           
+                            goflag = true;
+                        }
+                        break;
+                    case 2:  // left
+                        if ((x > 0) & (grid.gridRec[rw, cl].getLeft()))
+                        {
+                            cr = new Rectangle(x - 20, y, 40, 20);                           
+                            goflag = true;
+                        }
+                        break;
+                    case 3:  // right
+                        if ((x < 480) & (grid.gridRec[rw, cl].getRight()))
+                        {
+                            cr = new Rectangle(x, y, 40, 20);                           
+                            goflag = true;
+                        }
+                        break;
+                        
+                }
+                if (goflag == false) direction = ran.Next(4);
+            } while (goflag == false);
+           
+                Graphics g = panel1.CreateGraphics();
+                        
+    //            if (toggle == true) toggle = false;
+    //            else toggle = true;
+                panel1.Invalidate(cr);
+                panel1.Update();
+            
+            
+        } // end of timer definition
+                
+    }
+}
+
+ 
+      
---
+++ some motion 642012/Test1/GRectangle.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Drawing;
+using System.Text;
+//
+// May, 2012, J. Loftus, for Luzerne County Comunity College.   
+// Code to test and work with recursive backtracking for random
+// maze generated.  Part of a weekly workshop offered during the Summer 2012 entitled "Applied Data Structures
+// and Algorithms for Interactive Simulation systems.  
+// The GRectangle class wraps around the C#- provided Rectangle class, and includes some extra fields
+// and methods needed for a potential game.  It represents one 'square' on a 10 by 10 game grid.  
+
+namespace Test1
+{
+    class GRectangle 
+    {
+        private bool wall;
+        private Rectangle thisRec;
+        private List<GRectangle> Neighbors;
+        private int ivalue;
+        private int jvalue;
+        private int counter;
+        
+        private bool Up, Down, Left, Right;  // If current square has these neighbors
+        public GRectangle(int X, int Y, int Width, int Length, bool Wall, int I, int J)
+        {
+            Up = false; Down = false; Left = false; Right = false; 
+            counter = 0;
+            // The boolean variable wall indicates whether this current GRectangle is a wall (can't pass through) 
+            // or not. 
+            wall = Wall;
+            
+            // thisRec is just the C# Rectangle corresponding to this GRectangle. 
+            thisRec = new Rectangle(X, Y, Width, Length);
+
+            // Neighbors are the (up to 4) squares that are adjacent to the current one. 
+            Neighbors = new List<GRectangle>();
+            ivalue = I;
+            jvalue = J;
+        }
+        public GRectangle() { wall = true; }
+        public List<GRectangle> getNeighbors()
+        {
+            return Neighbors;
+        }
+        public void setUp(GRectangle u) 
+        {
+            Up = true; 
+            Neighbors.Add(u);
+        
+        }
+        public void setDown(GRectangle u)
+        {
+             Down = true;
+             Neighbors.Add(u);
+        }
+        public void setLeft(GRectangle u)
+        {
+            Left = true;
+            Neighbors.Add(u);
+        }
+        public void setRight(GRectangle u)
+        {
+            Right = true;
+            Neighbors.Add(u);
+        }
+       
+        public bool getUp() {return Up;}
+        public bool getDown() {return Down;}
+        public bool getLeft() {return Left;}
+        public bool getRight() {return Right;}
+        // At times we have to cast is back to Rectangle, to use various C# supplied drawing routines, etc.
+        public Rectangle toRectangle()
+        {
+            return thisRec;
+        }
+
+        public bool isWall()
+        {
+            return wall;
+        }
+        public void makeFree()
+        {
+            wall = false;
+        }
+
+        // getCount will return the number of neighbors this square has (a number from 2 to 4 inclusively)
+        public int getCount()
+        {
+            return Neighbors.Count;
+        }
+        // getCounter is temporary and will just house the order in which the squares are drawn to the screen.
+        // 
+        public int getCounter()
+        {
+            return counter;
+        }
+        public void setCounter(int callcounter)
+        {
+            counter = callcounter;
+        }
+        // is Available is used by the recursive search routine to see if the square currently being
+        // considered can be made 'free', i.e., not a wall.  We simply loop through its neighbors, 
+        // and count how many are already free.  If this number is two or more, it can't be used
+        //
+        public bool isAvailable()
+        {
+            int count = 0;
+            foreach (GRectangle n in Neighbors)
+            {
+                if (!n.isWall()) count++; 
+            }
+            if (count < 2) return true;
+            return false;
+        }
+    }
+
+}
+
