efc43f61a0ba1c4f096465a47787591891189a01
John Loftus <jloftus@luzerne.edu>
2012-06-09 17:30:45 -0400

    First commit of random maze via Kruskal's alg

---
+++ Disjoint union/Test1/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//
+// 6/2012 J. Loftus, for Luzerne County Community College
+// Code to generate a maze based on the Disjoint Union/Find algorithm (based on Kruskal's
+// algorithm for generating a spanning tree).
+// Part of a weekly workshop offered during the Summer 2012 entitled "Applied Data Structures
+// and Algorithms for Interactive Simulation systems.  
+//
+namespace Test1
+{
+    public partial class Form1 : Form
+    {
+        Graphics paper;
+  //        Grid grid = new Grid();
+        Grid grid;
+        public Random rand;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // When the panel in which the Grid of squares is being drawn is hit with a Point event,
+        // then we call drawGrid to redraw the maze. 
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            paper = e.Graphics;
+            grid = new Grid();
+            grid.drawGrid(paper);
+        }
+         
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+            panel1.Refresh();
+        }
+        
+        private void label2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Goodbye!");
+            Close();
+        }
+                
+    }
+}
+
+ 
+      
---
+++ Disjoint union/Test1/Pair.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+//
+// This is a virtual class, holding a pair of objects of a specific type.  
+// It is used in the Grid class to hold a GRectangle, integer pair.  
+namespace Test1
+{
+    public class Pair<T, U> 
+    { 
+    public Pair() { } 
+    public Pair(T first, U second) 
+    {   
+        this.First = first; 
+        this.Second = second; 
+    } 
+    public T First { get; set; } 
+    public U Second { get; set; } }; 
+    
+}
